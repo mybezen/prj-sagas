96692d8803168a827a2260acea00e3e44b144774
Fdhlnnn <fadhlan.aghnatan05@gmail.com>
2024-08-15 08:20:31 +0700

    Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import { ButtonDefault } from "../components/Button"
 import { DrawerWithForm } from "../components/Drawer"
 import { FooterWithSocialLinks } from "../components/Footer"
+import { NavbarSimple } from "../components/Navbar"
 
 function Home() {
   return (
     <div className="container mt-5 ">
+      <NavbarSimple/>
       <ButtonDefault /> 
+      <div className="mt-5 mb-5 items-center">
       <DrawerWithForm />
+      </div>
       <FooterWithSocialLinks />
     </div>
   )
