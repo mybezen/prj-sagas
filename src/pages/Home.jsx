ab778cd34055701207af459f620c49febf9b02a9
awiipp <sayidariipp@gmail.com>
2024-08-15 13:51:37 +0700

    home change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
-import { ButtonDefault } from "../components/Button"
-import { DrawerWithForm } from "../components/Drawer"
-import { FooterWithSocialLinks } from "../components/Footer"
-import { NavbarSimple } from "../components/Navbar"
+import { ButtonDefault } from '../components/Button';
+import { DrawerWithForm } from '../components/Drawer';
+import { FooterWithSocialLinks } from '../components/Footer';
+import { NavbarSimple } from '../components/Navbar';
 
 function Home() {
   return (
-    <div className="container mt-5 ">
-      <NavbarSimple/>
-      <ButtonDefault /> 
-      <div className="mt-5 mb-5 items-center">
-      <DrawerWithForm />
+    <div className="">
+      <NavbarSimple />
+      <div className="container mt-5 ">
+        <ButtonDefault />
+        <div className="mt-5 mb-5 items-center">
+          <DrawerWithForm />
+        </div>
+        <FooterWithSocialLinks />
       </div>
-      <FooterWithSocialLinks />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
