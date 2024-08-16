f0fce3fec9aa1a10b508e5f47b151ba0a2f9a0e8
awiipp <sayidariipp@gmail.com>
2024-08-16 10:57:34 +0700

    add lorem ipsum

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,110 @@ function Home() {
     <div className="">
       <StickyNavbar />
       <div className="container">
+        <div className="pb-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          totam corrupti ea quia unde delectus dolores quod hic ex distinctio.
+          Natus doloremque recusandae ipsa quo a aliquam sed deserunt quos
+          voluptas? Ullam temporibus nostrum quam modi sit, expedita cumque
+          quidem ducimus praesentium, qui, illum harum. Dolor reprehenderit quis
+          laboriosam. Iusto earum eaque tenetur amet porro. Saepe voluptas neque
+          rem officia molestias animi accusamus ex sed sunt, excepturi ratione
+          laudantium quibusdam id totam est minus rerum, ipsam distinctio vero
+          unde eius! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatem totam corrupti ea quia unde delectus dolores quod hic ex
+          distinctio. Natus doloremque recusandae ipsa quo a aliquam sed
+          deserunt quos voluptas? Ullam temporibus nostrum quam modi sit,
+          expedita cumque quidem ducimus praesentium, qui, illum harum. Dolor
+          reprehenderit quis laboriosam. Iusto earum eaque tenetur amet porro.
+          Saepe voluptas neque rem officia molestias animi accusamus ex sed
+          sunt, excepturi ratione laudantium quibusdam id totam est minus rerum,
+          ipsam distinctio vero unde eius! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Voluptatem totam corrupti ea quia unde
+          delectus dolores quod hic ex distinctio. Natus doloremque recusandae
+          ipsa quo a aliquam sed deserunt quos voluptas? Ullam temporibus
+          nostrum quam modi sit, expedita cumque quidem ducimus praesentium,
+          qui, illum harum. Dolor reprehenderit quis laboriosam. Iusto earum
+          eaque tenetur amet porro. Saepe voluptas neque rem officia molestias
+          animi accusamus ex sed sunt, excepturi ratione laudantium quibusdam id
+          totam est minus rerum, ipsam distinctio vero unde eius! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Voluptatem totam corrupti
+          ea quia unde delectus dolores quod hic ex distinctio. Natus doloremque
+          recusandae ipsa quo a aliquam sed deserunt quos voluptas? Ullam
+          temporibus nostrum quam modi sit, expedita cumque quidem ducimus
+          praesentium, qui, illum harum. Dolor reprehenderit quis laboriosam.
+          Iusto earum eaque tenetur amet porro. Saepe voluptas neque rem officia
+          molestias animi accusamus ex sed sunt, excepturi ratione laudantium
+          quibusdam id totam est minus rerum, ipsam distinctio vero unde eius!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          totam corrupti ea quia unde delectus dolores quod hic ex distinctio.
+          Natus doloremque recusandae ipsa quo a aliquam sed deserunt quos
+          voluptas? Ullam temporibus nostrum quam modi sit, expedita cumque
+          quidem ducimus praesentium, qui, illum harum. Dolor reprehenderit quis
+          laboriosam. Iusto earum eaque tenetur amet porro. Saepe voluptas neque
+          rem officia molestias animi accusamus ex sed sunt, excepturi ratione
+          laudantium quibusdam id totam est minus rerum, ipsam distinctio vero
+          unde eius! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatem totam corrupti ea quia unde delectus dolores quod hic ex
+          distinctio. Natus doloremque recusandae ipsa quo a aliquam sed
+          deserunt quos voluptas? Ullam temporibus nostrum quam modi sit,
+          expedita cumque quidem ducimus praesentium, qui, illum harum. Dolor
+          reprehenderit quis laboriosam. Iusto earum eaque tenetur amet porro.
+          Saepe voluptas neque rem officia molestias animi accusamus ex sed
+          sunt, excepturi ratione laudantium quibusdam id totam est minus rerum,
+          ipsam distinctio vero unde eius!
+        </div>
+        <div className="pb-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          totam corrupti ea quia unde delectus dolores quod hic ex distinctio.
+          Natus doloremque recusandae ipsa quo a aliquam sed deserunt quos
+          voluptas? Ullam temporibus nostrum quam modi sit, expedita cumque
+          quidem ducimus praesentium, qui, illum harum. Dolor reprehenderit quis
+          laboriosam. Iusto earum eaque tenetur amet porro. Saepe voluptas neque
+          rem officia molestias animi accusamus ex sed sunt, excepturi ratione
+          laudantium quibusdam id totam est minus rerum, ipsam distinctio vero
+          unde eius! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatem totam corrupti ea quia unde delectus dolores quod hic ex
+          distinctio. Natus doloremque recusandae ipsa quo a aliquam sed
+          deserunt quos voluptas? Ullam temporibus nostrum quam modi sit,
+          expedita cumque quidem ducimus praesentium, qui, illum harum. Dolor
+          reprehenderit quis laboriosam. Iusto earum eaque tenetur amet porro.
+          Saepe voluptas neque rem officia molestias animi accusamus ex sed
+          sunt, excepturi ratione laudantium quibusdam id totam est minus rerum,
+          ipsam distinctio vero unde eius! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Voluptatem totam corrupti ea quia unde
+          delectus dolores quod hic ex distinctio. Natus doloremque recusandae
+          ipsa quo a aliquam sed deserunt quos voluptas? Ullam temporibus
+          nostrum quam modi sit, expedita cumque quidem ducimus praesentium,
+          qui, illum harum. Dolor reprehenderit quis laboriosam. Iusto earum
+          eaque tenetur amet porro. Saepe voluptas neque rem officia molestias
+          animi accusamus ex sed sunt, excepturi ratione laudantium quibusdam id
+          totam est minus rerum, ipsam distinctio vero unde eius! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Voluptatem totam corrupti
+          ea quia unde delectus dolores quod hic ex distinctio. Natus doloremque
+          recusandae ipsa quo a aliquam sed deserunt quos voluptas? Ullam
+          temporibus nostrum quam modi sit, expedita cumque quidem ducimus
+          praesentium, qui, illum harum. Dolor reprehenderit quis laboriosam.
+          Iusto earum eaque tenetur amet porro. Saepe voluptas neque rem officia
+          molestias animi accusamus ex sed sunt, excepturi ratione laudantium
+          quibusdam id totam est minus rerum, ipsam distinctio vero unde eius!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          totam corrupti ea quia unde delectus dolores quod hic ex distinctio.
+          Natus doloremque recusandae ipsa quo a aliquam sed deserunt quos
+          voluptas? Ullam temporibus nostrum quam modi sit, expedita cumque
+          quidem ducimus praesentium, qui, illum harum. Dolor reprehenderit quis
+          laboriosam. Iusto earum eaque tenetur amet porro. Saepe voluptas neque
+          rem officia molestias animi accusamus ex sed sunt, excepturi ratione
+          laudantium quibusdam id totam est minus rerum, ipsam distinctio vero
+          unde eius! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatem totam corrupti ea quia unde delectus dolores quod hic ex
+          distinctio. Natus doloremque recusandae ipsa quo a aliquam sed
+          deserunt quos voluptas? Ullam temporibus nostrum quam modi sit,
+          expedita cumque quidem ducimus praesentium, qui, illum harum. Dolor
+          reprehenderit quis laboriosam. Iusto earum eaque tenetur amet porro.
+          Saepe voluptas neque rem officia molestias animi accusamus ex sed
+          sunt, excepturi ratione laudantium quibusdam id totam est minus rerum,
+          ipsam distinctio vero unde eius!
+        </div>
         <DrawerWithForm />
         <FooterWithSocialLinks />
       </div>
