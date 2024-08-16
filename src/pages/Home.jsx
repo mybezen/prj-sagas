058a7a8484cbb1a27c5edbe02edd22f5be9b24c9
awiipp <sayidariipp@gmail.com>
2024-08-16 13:59:08 +0700

    change Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import LandingPage from "../sections/LandingPage";
+import TextBlock from "../sections/TextBlock";
 
 function Home() {
   useEffect(() => {
@@ -16,6 +17,7 @@ function Home() {
   return (
     <div className="">
       <LandingPage />
+      <TextBlock />
       <StickyNavbar />
       <div className="container">
         <AccordionCustomAnimation />
