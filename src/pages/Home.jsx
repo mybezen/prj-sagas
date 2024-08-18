05be164ea7443cf97da4fdea44a0d9966271fbf1
awiipp <sayidariipp@gmail.com>
2024-08-18 15:27:14 +0700

    Edit TextBlock.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,37 @@
-import { FooterWithSocialLinks } from "../components/Footer";
-import { StickyNavbar } from "../components/Navbar";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
-import LandingPage from "../sections/LandingPage";
-import TextBlock from "../sections/TextBlock";
-import { SimpleCard } from "../components/Card";
-import IconLabelTabs from "../components/Tabs";
+import { FooterWithSocialLinks } from '../components/Footer';
+import { StickyNavbar } from '../components/Navbar';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import LandingPage from '../sections/LandingPage';
+import TextBlock from '../sections/TextBlock';
+import { SimpleCard } from '../components/Card';
+import IconLabelTabs from '../components/Tabs';
 
 function Home() {
   useEffect(() => {
     AOS.init({
       once: true,
       duration: 500,
-      mirror: true
+      mirror: true,
     });
   }, []);
   return (
     <div className="">
       <LandingPage />
       <StickyNavbar />
-      <div className="container items-center bg-green-900 rounded-lg my-4"  data-aos="fade-up"
-            data-aos-duration="1200"
-            data-aos-easing="ease-in-sine"
-            data-aos-anchor-placement="top-center">
-        <TextBlock />
-      </div>
+      <TextBlock />
       <div className="container bg-amber-400 flex flex-col items-center my-4">
-      <IconLabelTabs />
+        <IconLabelTabs />
       </div>
-      <div className="container flex flex-col items-center bg-stone-700 my-4 pt-24" data-aos="fade-up"
-     data-aos-duration="3000">
+      <div
+        className="container flex flex-col items-center bg-stone-700 my-4 pt-24"
+        data-aos="fade-up"
+        data-aos-duration="3000"
+      >
         <SimpleCard />
-        <div
-          className="mt-5 pb-10"
-        >
-       
-            Materi 1
-        </div>
-        <div className="pb-10">
-            Materi 2
-        </div>
+        <div className="mt-5 pb-10">Materi 1</div>
+        <div className="pb-10">Materi 2</div>
       </div>
       <FooterWithSocialLinks />
     </div>
--- src/sections/TextBlock.jsx
+++ src/sections/TextBlock.jsx
@@ -12,8 +12,36 @@ function TextBlock() {
     AOS.refresh();
   }, []);
   return (
-    <div id="textblock">
-      <div id="textblock-container"></div>
+    <div
+      className="w-full h-screen flex"
+      id="textblock-container"
+      data-aos="fade-up"
+      data-aos-duration="700"
+      data-aos-easing="ease-in-sine"
+      data-aos-anchor-placement="top-center"
+    >
+      <div className="lg:w-1/2 flex justify-center items-center lg:ml-24">
+        <div className="w-10/12">
+          <h1 className="text-5xl poppins-bold mb-3">
+            What is{' '}
+            <span className="text-dark-gr">
+              Vital<span className="text-lime-gr">Motion</span>
+            </span>
+            ?
+          </h1>
+          <p className="font-normal text-3xl">
+            <span className="text-dark-gr poppins-bold">
+              Vital<span className="text-lime-gr poppins-bold">Motion</span>
+            </span>{' '}
+            adalah situs web yang memberikan informasi tentang pentingnya
+            aktivitas fisik untuk kesehatan tubuh, risiko akibat kurangnya
+            aktivitas, dan cara pencegahannya.
+          </p>
+        </div>
+      </div>
+      <div className="lg:w-1/2 bg-slate-200 flex justify-center items-center">
+        <img src="#" alt="" className="w-96 h-96 bg-gray-700 rounded-full" />
+      </div>
     </div>
   );
 }
