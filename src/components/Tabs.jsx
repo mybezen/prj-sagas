12b3a7aac66ab5526b38eec29c49255482d6da67
mybezen <amriikhda207@gmail.com>
2024-08-18 14:49:05 +0700

    Update Tabs

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -28,7 +28,10 @@ export default function IconLabelTabs() {
 
         {value === 0 && <Box className='p-4'>Obesitas adalah kondisi medis berupa berat badan di atas normal karena penumpukan lemak berlebih. Hal tersebut terjadi karena asupan kalori lebih tinggi dibandingkan dengan kalori yang digunakan. Apabila kondisi ini terjadi dalam kurun waktu lama, besar kemungkinan berat badan akan terus bertambah.</Box>}
         {value === 1 && <Box className='p-4'>Transient Ischemic Attack (TIA) atau lebih dikenal dengan stroke ringan adalah serangan yang muncul secara tiba-tiba berupa gejala stroke, tetapi sifatnya sementara atau dalam waktu singkat. Kebanyakan stroke ringan adalah kondisi yang akan bertahan selama beberapa menit hingga jam, kemudian mereda dalam waktu 24 jam.</Box>}
-        {value === 2 && <Box className='p-4'>Hipertensi adalah istilah medis ketika kondisi tekanan darah pada arteri meningkat melebihi batas normal. Tekanan darah terbagi menjadi dua bagian, yaitu sistolik dan diastolik.</Box>}
+        {value === 2 && <Box className='p-4'>Hipertensi adalah istilah medis ketika kondisi tekanan darah pada arteri meningkat melebihi batas normal. Tekanan darah terbagi menjadi dua bagian, yaitu sistolik dan diastolik.
+
+Tekanan sistolik adalah tekanan ketika jantung memompa darah keseluruh tubuh. Sedangkan tekanan diastolik adalah tekanan ketika jantung berada dalam kondisi rileks saat menerima darah kembali ke ruang jantung sebelum kembali memompakannya ke seluruh tubuh. Hipertensi ditegakkan saat kondisi tekanan sistolik melebihi atau sama dengan 140 mmHg dan diastolik melebihi atau sama dengan 90 mmHg. 
+</Box>}
         {value === 3 && <Box className='p-4'>Gagal ginjal adalah suatu kondisi penurunan fungsi ginjal dalam menyaring limbah sisa metabolisme tubuh dari dalam darah dan membuangnya melalui urin. Kondisi tersebut menyebabkan kadar racun dan cairan berbahaya akan mengendap dalam tubuh dan dapat berakibat fatal apabila tidak ditangani.</Box>}
         {value === 4 && <Box className='p-4'>Osteoporosis adalah kondisi ketika kepadatan tulang berkurang sehingga tulang menjadi keropos dan mudah patah. Osteoporosis jarang menimbulkan gejala dan biasanya baru diketahui ketika penderitanya jatuh atau mengalami cedera yang menyebabkan patah tulang.</Box>}
     </Box>
