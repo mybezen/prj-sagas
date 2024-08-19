8bb3902c2eda17b8e795fa0e71b592c47ccdf271
Fdhlnnn <fadhlan.aghnatan05@gmail.com>
2024-08-19 16:16:53 +0700

    Add Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import { Card, CardBody } from '@material-tailwind/react';
-import { AccordionCustomAnimation, AccordionPencegahan1, AccordionPencegahan2 } from './Accordion';
+import { AccordionCustomAnimation, AccordionPencegahan1, AccordionPencegahan2, AccordionPencegahan3, AccordionPencegahan4 } from './Accordion';
 
 export function PencegahanCard1() {
   return (
@@ -20,6 +20,25 @@ export function PencegahanCard2() {
     </Card>
   );
 }
+export function PencegahanCard3() {
+  return (
+    <Card className="w-9/12">
+      <CardBody>
+        <AccordionPencegahan3 />
+      </CardBody>
+    </Card>
+  );
+}
+export function PencegahanCard4() {
+  return (
+    <Card className="w-9/12">
+      <CardBody>
+        <AccordionPencegahan4 />
+      </CardBody>
+    </Card>
+  );
+}
+
 
 export function StudCard() {
   return (
