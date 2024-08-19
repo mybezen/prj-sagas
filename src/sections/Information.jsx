8e5e849f9057999378d72ad74d40215a79ef1f97
mybezen <amriikhda207@gmail.com>
2024-08-19 19:08:04 +0700

    Fix Accordion And Layout Information

--- src/sections/Information.jsx
+++ src/sections/Information.jsx
@@ -2,8 +2,8 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import { PencegahanCard1, PencegahanCard2, PencegahanCard3, PencegahanCard4 } from '../components/Card';
-import CloudIcon from '../assets/CloudBottom.svg'
-import DoctIcon from '../assets/HaloDog.svg'
+import CloudIcon from '../assets/CloudBottom.svg';
+import DoctIcon from '../assets/HaloDog.svg';
 
 function Information() {
   useEffect(() => {
@@ -15,17 +15,18 @@ function Information() {
   }, []);
 
   return (
-    <div>
-      <img className='absolute' src={CloudIcon} alt="" />
-      <div
-        className="container pt-[32rem] pb-16 flex"
+    <div className='flex flex-col'>
+      <img className='absolute w-full' src={CloudIcon} alt="" />
+      <div className="container pt-[32rem] pb-16 flex justify-center items-center"
         data-aos="fade-up"
-        data-aos-duration="700"
-      >
-        <div>
-          <img src={DoctIcon} alt="" />
+        data-aos-duration="700">
+        {/* Left Side: DoctIcon */}
+        <div className="flex-none">
+          <img src={DoctIcon} alt="Doctor Icon" className="max-w-full h-auto" />
         </div>
-        <div className='flex flex-col items-center p-[3rem]'>
+        
+        {/* Right Side: Cards */}
+        <div className='flex flex-col items-center p-[3rem] space-y-6'>
           <PencegahanCard1 />
           <PencegahanCard2 />
           <PencegahanCard3 />
