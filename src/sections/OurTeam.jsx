69c8feea6cd58ebeb0224c3cb2b5c68c3c152bef
awiipp <sayidariipp@gmail.com>
2024-08-20 22:59:40 +0700

    Footer.

--- src/sections/OurTeam.jsx
+++ src/sections/OurTeam.jsx
@@ -1,22 +1,23 @@
-import { CarouselCustomArrows } from "../components/CardCarousel";
+import { CarouselCustomArrows } from '../components/CardCarousel';
 import TogetherIcon from '../assets/TogethIcon.svg';
 
 function OurTeam() {
   return (
-    <div className="relative flex flex-col items-center mt-4"
-          style={{
-            backgroundImage: `url(${TogetherIcon})`, // Mengatur gambar latar belakang
-        backgroundSize: 'cover', // Mengatur gambar agar menutupi seluruh div
-        backgroundPosition: 'center', // Menempatkan gambar di tengah div
-        backgroundRepeat: 'no-repeat', // Mencegah pengulangan gambar
-        width: '100%', // Menyesuaikan lebar div
-        height: '100vh' // Menyesuaikan tinggi div dengan tinggi viewport
-          }}
+    <div
+      className="flex flex-col items-center mt-4"
+      style={{
+        backgroundImage: `url(${TogetherIcon})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100vh',
+      }}
     >
-      <h1 className="pt-5 text-2xl poppins-bold">Our Team</h1>
-
-      {/* Container Carousel dengan Padding */}
-      <div className="flex justify-center w-full pt-[7rem] ">
+      <h1 className="pt-5 text-5xl poppins-bold mt-20 text-zinc-800">
+        Our Team
+      </h1>
+      <div className="flex justify-center w-full pt-[7rem] h-screen -mt-24">
         <CarouselCustomArrows />
       </div>
     </div>
